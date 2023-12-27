68c3fafd30bee03271f9969be190c8bccbe76f49
João Victor Escudeiro <88864846+Overplayed1@users.noreply.github.com>
2023-12-27 18:18:05 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+let listaDeNumerosSorteados = [];
+let limiteNumeros = 10;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+exibirMensagemInicial();
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+
+    if (chute == numeroSecreto) {
+        escreverTexto('h1', 'VOCÊ ACERTOU!');
+        let mensagemTentativas = `Parabéns! Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
+        escreverTexto('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            escreverTexto('p', 'O número secreto é menor');
+        } else {
+            escreverTexto('p', 'O número secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function escreverTexto(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+}
+
+
+function exibirMensagemInicial() {
+    escreverTexto('h1', 'Jogo do número secreto');
+    escreverTexto('p', 'Escolha um número entre 1 e 10');
+}
+
+function gerarNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random() * limiteNumeros + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+    if (quantidadeDeElementosNaLista == limiteNumeros){
+        listaDeNumerosSorteados = [];
+    }
+
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)){
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        return numeroEscolhido;
+    }
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled',true);
+}
